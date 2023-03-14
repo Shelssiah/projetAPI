0a022eb3e32a197103f392fd7112ee5ed28e6f8f
Shelssiah <shelmyassiah@gmail.com>
2023-03-14 21:42:11 +0100

    ajout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // packages requis
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs= require('fs');
 
 // port
 const PORT = process.env.PORT || 3000;
@@ -14,18 +15,33 @@ app.use(express.static('client'));
 
 // verification
 app.get('/', (request, response) => {
-    // The string we want to display on http://localhost:3000
     response.send('Welcome on the annotations API! Hello Wold ! ');
 });
 
+// def de var
+let iden= 0;
 
 // root
 app.post('/annotation', (req, res) => {
   const annotation = {
+    id: iden+1,
     url: req.body.url,
     comment: req.body.comment,
     note: req.body.note
   };
+
+  // fichier existant
+
+  const data= fs.raedFileSync('annot.json','utf8');
+  const val= JSON.parse(data);
+
+  // ajout nouv créé
+  val.push(annotation);
+
+  //enregistrement
+  fs.writeFileSync('annot.json', JSON.stringify(val));
+
+
   res.json(annotation);
 });
 
