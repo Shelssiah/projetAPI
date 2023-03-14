18cc6d380f25f98192021c6ba978d47f2e94d273
Shelssiah <shelmyassiah@gmail.com>
2023-03-14 14:54:06 +0100

    ajout

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
-/*var express = require('express');
-var app = express();
+// packages requis
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const port = 3000;
+// port
+const PORT = process.env.PORT || 3000;
 
-//const bodyParser = require('body-parser');
 
-app.get("/toto",function(req, res){
+// appels
+const app = express();
+app.use(bodyParser.json());
 
-	res.send("Bonjour Toto");
+// root
+app.post('/annotation', (req, res) => {
+  const annotation = {
+    url: req.body.url,
+    comment: req.body.comment,
+    note: req.body.note
+  };
+  res.json(annotation);
 });
 
-app.listen(port, function(){
-	console.log('serveur listening on port: '+port);
+// serveur
+app.listen(PORT, () =>{
+  console.log(`The Annotations API is running on: http://localhost:${PORT}.`);
+});
+
+/*
+//
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/annotation', (req, res) => {
+  const annotation = {
+    url: req.body.url,
+    comment: req.body.comment,
+    note: req.body.note
+  };
+  res.json(annotation);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 */
 
-const express = require('express');
-const app = express();
-
-// This variable defines the port of your computer where the API will be available
-const PORT = process.env.PORT || 3000;
-
+/* # test fonctionnel
 app.get('/', (request, response) => {
     // The string we want to display on http://localhost:3000
     response.send('Welcome on the annotations API! Hello Wold ! ');
 });
-
-app.listen(PORT, () =>{
-  console.log(`The Annotations API is running on: http://localhost:${PORT}.`);
-});
+*/
